8f853b213c116a83eba8a6694eb70706639e83c5
Carolina Lopes <ccal2@cin.ufpe.br>
2021-11-02 18:55:30 -0300

    Adicionar interface do repositorio do colaborador

---
+++ back-end/Usuario/Colaborador/IRepositorioColaborador.py
@@ -0,0 +1,21 @@
+import abc
+from typing import Optional
+from Colaborador import Colaborador
+
+class IRepositorioColaborador(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def inserir(self, colaborador: Colaborador):
+        return
+
+    @abc.abstractmethod
+    def consultarColaborador(self, email: str) -> Optional[Colaborador]:
+        return
+
+    @abc.abstractmethod
+    def atualizar(self, colaborador: Colaborador):
+        return
+
+    @abc.abstractmethod
+    def remover(self, colaborador: Colaborador):
+        return
