607b64d1e4d3eac1125914b61fb0aab9b7821de7
Carolina Lopes <ccal2@cin.ufpe.br>
2021-11-02 19:22:46 -0300

    Atualizar repositório do colaborador

--- back-end/Usuario/Colaborador/Colaborador.py
+++ back-end/Usuario/Colaborador/Colaborador.py
@@ -1,10 +1,11 @@
 from Usuario import Usuario
+from Interesse.Interesse import Interesse
 
 
 class Colaborador(Usuario):
 
-    def __init__(self, email: str, nome: str, area: str, cargo: str):
+    def __init__(self, email: str, nome: str, area: str, cargo: str, interesses: list(Interesse)=[]):
         super().__init__(email, nome)
         self.area = area
         self.cargo = cargo
-        self.interesses = []
+        self.interesses = interesses
