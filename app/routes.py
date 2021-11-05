8c54c11f5c1c8c46f23ca0074c87438cb495e538
Gabriela Leal <gabriela@vinta.com.br>
2021-11-04 21:05:09 -0300

    Adicionar rotas para paginas

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,6 @@ from model.usuario.colaborador.repositorio_colaborador_firestore import Reposito
 
 bp = Blueprint("routes", __name__)
 
-
 @bp.route("/consultar", methods=["GET"])
 def consultar():
     repo = RepositorioColaboradorFirestore()
@@ -12,3 +11,17 @@ def consultar():
     colaborador = repo.consultar_colaborador(email="ccal2@cin.ufpe.br")
 
     return render_template("consulta_colaborador.html", colaborador=colaborador)
+
+@bp.route("/inicio/colaborador", methods=["GET"])
+def inicio_colaborador():
+    return render_template("inicio_colaborador.html")
+
+@bp.route("/inicio/administrador", methods=["GET"])
+def inicio_administrador():
+    return render_template("inicio_administrador.html")
+
+@bp.route("/recomendacoes/colaborador", methods=["GET"])
+def recomendacoes_colaborador():
+    # precisa chamar o metodo que vai pegar o usuario logado e gerar as recomendacoes
+    # depois passar pro template
+    return render_template("recomendacoes_sistema.html")
