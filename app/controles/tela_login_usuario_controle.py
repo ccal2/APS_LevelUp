3e1c3f8eace00788a711d8434bd9fb20150b265a
Gabriela Leal <gabriela@vinta.com.br>
2021-11-06 11:22:54 -0300

    Implementaçao login

--- app/controles/tela_login_usuario_controle.py
+++ app/controles/tela_login_usuario_controle.py
@@ -1,6 +1,26 @@
+from model.fachadas.fachada import Fachada
+from model.usuario.administrador.administrador import Administrador
+from model.usuario.colaborador.colaborador import Colaborador
+
 class TelaLoginUsuarioControle:
-    def __init__(self, fachada):
-        self.fachada = fachada
+    def __init__(self):
+        self.fachada = Fachada()
+        self.tela = "TelaLoginUsuario.html"
 
     def realizar_login(self, email, senha):
-        ...
+        resultado_login = self.fachada.realizar_login(email=email, senha=senha)
+        resposta = {}
+        # se não for None, pode ser um erro, um Administrador ou um Colaborador
+        if resultado_login is None:
+            resposta["erro"] = "Usuário não encontrado"
+        elif type(resultado_login) is Administrador:
+            resposta["redirecionar"] = "TelaInicioAdministrador.html"
+        elif type(resultado_login) is Colaborador:
+            resposta["redirecionar"] = "TelaInicioColaborador.html"
+        elif resultado_login.get("status") == "error":
+            if resultado_login.get("message") == "INVALID_PASSWORD":
+                resposta["erro"] = "Senha inválida"
+            elif resultado_login.get("message") == "EMAIL_NOT_FOUND":
+                resposta["erro"] = "Email não cadastrado"
+
+        return resposta
