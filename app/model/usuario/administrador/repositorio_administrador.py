dd9aca6d98f7ac15b4898e1ded06af05bc66b1d9
Carolina Lopes <ccal2@cin.ufpe.br>
2021-11-05 22:00:22 -0300

    Adicionar repositorio e conversor para administrador

---
+++ app/model/usuario/administrador/repositorio_administrador.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from firebase_admin import firestore
+from utils.constantes import *
+
+from model.usuario.administrador.administrador import Administrador
+from model.usuario.administrador.i_repositorio_administrador import IRepositorioAdministrador
+from model.conversores.conversor_administrador_dicionario import ConversorAdministradorDicionario
+
+
+class RepositorioAdministradorFirestore(IRepositorioAdministrador):
+    def __init__(self):
+        self.colecao = firestore.client().collection(DB_ADMINISTRADORES)
+
+    def consultar_administrador(self, email: str) -> Optional[Administrador]:
+        referencia = self.colecao.document(email)
+        documento = referencia.get()
+
+        if not documento.exists:
+            return None
+
+        dicionario = documento.to_dict()
+        administrador = ConversorAdministradorDicionario.dicionario_para_administrador(dicionario)
+
+        return administrador
