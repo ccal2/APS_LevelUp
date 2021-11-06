5d1b6d114d78067dfe5150aa1f2d0c4802450de4
rosig <rglj2@cin.ufpe.br>
2021-11-06 11:38:31 -0300

    Adicionando controle recomendacoes

--- app/model/fachadas/fachada.py
+++ app/model/fachadas/fachada.py
@@ -1,8 +1,14 @@
 from model.controladores.controlador_login_usuario import ControladorLoginUsuario
+from model.controladores.controlador_recomendacoes import ControladorRecomendacoes
+from model.usuario.colaborador.colaborador import Colaborador
 
 class Fachada:
     def __init__(self):
         self.controlador_login_usuario = ControladorLoginUsuario()
+        self.controlador_recomendacoes = ControladorRecomendacoes()
 
     def realizar_login(self, email: str, senha: str):  #  -> Optional[Usuario] | str
         return self.controlador_login_usuario.realizar_login(email, senha)
+    
+    def solicitar_recomendacoes(self, colaborador: Colaborador):
+        return self.controlador_recomendacoes.solicitar_recomendacoes(colaborador)
