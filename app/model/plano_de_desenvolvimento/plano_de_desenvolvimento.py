65b6b6683cf3c8c0badbf8eb0e01950831b4d9d4
Carolina Lopes <ccal2@cin.ufpe.br>
2021-11-06 14:35:33 -0300

    Renomear propriedade do plano de desenvolvimento

--- app/model/plano_de_desenvolvimento/plano_de_desenvolvimento.py
+++ app/model/plano_de_desenvolvimento/plano_de_desenvolvimento.py
@@ -1,4 +1,4 @@
 class PlanoDeDesenvolvimento:
-    def __init__(self, email_colaborador: str, ids_habilidades: "list[str]" = []):
-        self.email_colaborador = email_colaborador
+    def __init__(self, id_colaborador: str, ids_habilidades: "list[str]" = []):
+        self.id_colaborador = id_colaborador
         self.ids_habilidades = ids_habilidades
