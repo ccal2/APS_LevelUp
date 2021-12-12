f782c621826c77c5ef11eb0f08c7b72906b47eb0
rosig <rglj2@cin.ufpe.br>
2021-12-12 10:11:34 -0300

    Add telas de inicio e habilidades

--- SOA/servico-front/routes.py
+++ SOA/servico-front/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, request
+from flask import Blueprint, render_template, redirect, request, url_for
 
 # from controles.tela_login_usuario_controle import TelaLoginUsuarioControle
 # from controles.tela_recomendacoes_do_sistema_controle import TelaRecomendacoesDoSistemaControle
@@ -9,10 +9,30 @@ bp = Blueprint("routes", __name__)
 # controleRecomendacoes = TelaRecomendacoesDoSistemaControle()
 # controleLoginUsuario = TelaLoginUsuarioControle()
 
+@bp.route("/", methods=["GET"])
+def tela_redirecionamento_inicial():
+    return redirect(url_for('routes.tela_login_usuario_controle'))
+    
+@bp.route("/login", methods=["GET", "POST"])
+def tela_login_usuario_controle():
+    # erro = None
+    # if request.method == "POST":
+    #     resultado = controleLoginUsuario.realizar_login(request.form["email"], request.form["senha"])
+    #     if resultado["status"] == "erro":
+    #         erro = resultado["mensagem"]
+    #     elif resultado["status"] == "sucesso":
+    #         return redirect("/inicio")
+    # return render_template(controleLoginUsuario.tela, erro=erro)
+    return render_template("TelaLoginUsuario.html")
 
-# @bp.route("/inicio", methods=["GET"])
-# def tela_inicio_controle():
-#     return render_template("TelaInicio.html")
+@bp.route("/inicio", methods=["GET"])
+def tela_inicio_controle():
+    return render_template("TelaInicio.html")
+
+
+@bp.route("/habilidades", methods=["GET"])
+def tela_habilidades():
+    return render_template("TelaHabilidades.html")
 
 
 # @bp.route("/recomendacoes/", methods=["GET"])
@@ -25,16 +45,3 @@ bp = Blueprint("routes", __name__)
 #     elif resultado["status"] == "sucesso":
 #         habilidades = resultado["recomendacoes"]
 #     return render_template("TelaRecomendacoesDoSistema.html", erro=erro, habilidades=habilidades)
-
-
-@bp.route("/login", methods=["GET", "POST"])
-def tela_login_usuario_controle():
-    # erro = None
-    # if request.method == "POST":
-    #     resultado = controleLoginUsuario.realizar_login(request.form["email"], request.form["senha"])
-    #     if resultado["status"] == "erro":
-    #         erro = resultado["mensagem"]
-    #     elif resultado["status"] == "sucesso":
-    #         return redirect("/inicio")
-    # return render_template(controleLoginUsuario.tela, erro=erro)
-    return render_template("TelaLoginUsuario.html")
