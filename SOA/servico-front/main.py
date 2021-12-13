ae94995a9e78325d5c04dd0e8691b7df9418b9dc
Carolina Lopes <ccal2@cin.ufpe.br>
2021-12-12 21:47:41 -0300

    Adicionar secret key ao app do Flask

--- SOA/servico-front/main.py
+++ SOA/servico-front/main.py
@@ -1,12 +1,17 @@
-# Required imports
 import os
 
 from flask import Flask
 from routes import bp
+from dotenv import load_dotenv
+
+
+load_dotenv()
+FLASK_APP_SECRET_KEY = os.getenv("FLASK_APP_SECRET_KEY")
 
 # Initialize Flask app
 app = Flask(__name__)
 app.register_blueprint(bp)
+app.secret_key = FLASK_APP_SECRET_KEY
 
 port = int(os.environ.get("PORT", 5003))
 
